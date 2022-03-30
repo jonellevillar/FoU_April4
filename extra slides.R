d41803e9911793827af6b7af934a6b4e8e39b142
Jonelle Villar <jonellevillar@gmail.com>
2022-03-30 23:18:07 +0200

    delete

---
+++ extra slides.R
@@ -0,0 +1,18 @@
+```{r, correlation}
+
+#Correlation between plasma levels and brain for antipsychotic drugs (Hiemke, 2018)
+
+#Correlation between blood and brain DMPS (Braun, 2019)
+
+
+
+```
+
+```{r}
+
+#Need to correct for age and sex which affects cell-type compostion, but also serum concentrations, metabolic enzymes, and has been shown to be associated with adherance and compliance. 
+
+#We are not considering ethnicity as we have selected European patients. We have not as yet addressed genotypes in relation to the enzymes but have this data.
+
+
+```
