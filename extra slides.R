9f8a4beaef0ade276032d16c96aa303039165523
jonellevillar <jonellevillar@gmail.com>
2022-04-01 20:00:18 +0200

    extra slides

--- extra slides.R
+++ extra slides.R
@@ -22,3 +22,68 @@ background-position:50% 70%
 
 class: top left
 ### **DNA methylation** <br />
+
+
+
+<!--end of pull left-->
+  
+  
+  
+  ### Background: Differences <br> <br>
+  
+  .bitty[
+    - Polygenic risk score (PRS) differed significantly (Ruderfer, _et al._, 2014) <br> <br>
+      - Two genome-wide significant SNPs (BPD & SCZ Working Group of the PGC., 2018) <br> <br>
+      - Clinical differences behavioral, cognition, intelligence (Jabben,_et al._, 2010, Parellada, _et al._, 2017) <br> <br> 
+  ]
+
+
+----
+  
+  ###Background: Epigenetics in SCZ & BPD  <br> <br> 
+  
+  .bitty[
+    - Development: prenatal environmental factors _in utero_ <br> <br>
+      - MZ twins discordant for SCZ/BPD (Dempster, _et al._, 2011) <br> <br>
+      - Dynamic; many environmental exposures - trauma, adversity, life-style <br> <br>
+      - Associated with brain volume, structure and function; <br> <br>
+      - social and cognitive function (Starnawska & Demontis, 2021) <br> <br>
+      - DNAm changes overtime as a function of age and chronicity of the disorder 
+  ]
+
+---
+  
+  
+  
+  ###Background: Characteristics of SCZ & BPD <br> <br>
+  
+  ###Similarities
+  
+  .bitty[
+    - Early age of onset ~ 20-30 years old <br> <br>
+      - Heritability ~ 80% <br> <br>
+      - Periods of stability and relapse <br> <br>
+      - Dysregulated circadian rhythmicity: both a trait and state marker (Walker II, _et al._, 2020) <br> <br>
+      - Medication primary form of treatment 
+  ]
+
+---
+  
+  
+  <video src="[url]">[alt]</video>
+  
+  
+  
+  ![video](slides/osc_modC.mp4)
+
+
+```{r xaringanExtra-freezeframe, echo=FALSE}
+xaringanExtra::use_freezeframe()
+```
+
+
+class: top left 
+### Oscillating CpGs
+
+.center[![modC](slides/osc_modC.gif)]
+
